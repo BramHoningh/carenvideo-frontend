7e244d05a24d9f7bebcc8a55332ddc69d9ba9df8
MrPAttema <patrickattema@home.nl>
2018-05-23 15:32:37 +0200

    Base call component

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import GetAccessToken from './components/GetAccessToken.vue'
+import InitCall from './components/InitCall.vue'
+
 
 Vue.use(Router)
 
@@ -17,6 +19,11 @@ export default new Router({
       path: '/oauth',
       name: 'oauth',
       component: GetAccessToken
+    },
+    {
+      paht: '/call',
+      name: 'call',
+      component: InitCall
     }
   ]
 })
