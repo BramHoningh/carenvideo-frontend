318b92d9157354e9e203205cbafa3ed0e0e1aae4
Pixeldenker <bram@pixeldenker.nl>
2018-05-23 11:19:13 +0200

    Install axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="carenLogin"></button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+  },
+  methods: {
+    carenLogin () {
+      
+    }
   }
 }
 </script>
