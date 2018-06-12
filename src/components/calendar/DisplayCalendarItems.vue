71cb9ad0e4b45d855e48ab1de210cae8d496de43
Pixeldenker <bram@pixeldenker.nl>
2018-06-12 18:54:11 +0200

    Get calendar items from db

---
+++ src/components/calendar/DisplayCalendarItems.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+import variables from '../../variables'
+
+export default {
+  name: 'DisplayCalendarItems',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
