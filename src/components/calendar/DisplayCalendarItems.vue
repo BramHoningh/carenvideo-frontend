246a52a82675e8e28225d7a386a1fe05523bd68d
Pixeldenker <bram@pixeldenker.nl>
2018-06-13 09:49:37 +0200

    Add edit functionality for calendar items

--- src/components/calendar/DisplayCalendarItems.vue
+++ src/components/calendar/DisplayCalendarItems.vue
@@ -2,8 +2,10 @@
 <div class="calendar-display">
   <div class="calendar-item" v-for="(calendarItem, index) in calendarItems" :key="index">
     {{calendarItem.title}} | {{calendarItem.description}} | {{formatDate(calendarItem.startDate)}} |
-    <button class="button-primary">Wijzigen</button> | <button class="button-primary">Verwijderen</button>
+    <button class="button-primary" @click="openEditModal(calendarItem)">Wijzigen</button> | <button class="button-primary">Verwijderen</button>
   </div>
+
+  <EditCalendarItems :show="showEdit" :calendarItem="editCalendarItem" />
 </div>
 </template>
 
@@ -11,13 +13,28 @@
 import axios from 'axios'
 import moment from 'moment'
 import variables from '../../variables'
+import EditCalendarItems from './EditCalendarItem'
 
 export default {
   name: 'DisplayCalendarItems',
   props: ['calendarItems'],
+  components: {
+    EditCalendarItems
+  },
+  data () {
+    return {
+      editCalendarItem: {},
+      showEdit: false
+    }
+  },
   methods: {
     formatDate (date) {
       return moment(date).format('DD MMMM YYYY, HH:mm')
+    },
+
+    openEditModal (item) {
+      this.editCalendarItem = item
+      this.showEdit = true
     }
   },
   created () {
