58831fcc124b1d45273743801304a679f9222297
Pixeldenker <bram@pixeldenker.nl>
2018-06-13 11:43:12 +0200

    Fix issue with fetching calendar data

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -42,20 +42,18 @@ export default {
     },
 
     getCalendarItems (id) {
-      axios({
-        method: 'GET',
-          url: variables.getCalendarItemsEndpoint,
-          headers: {'Content-Type': 'application/json'},
-          params: {
-            user_id: id,
-          }
+      axios.get(variables.getCalenderItemsEndpoint, {
+        headers: { 'Content-Type': 'application/json' },
+        params: {
+          user_id: id
+        }
       })
       .then(response => {
-        console.log(response.data.data.items)
+        console.log(response)
         this.calendarData = response.data.data.items
       })
-      .catch(err => {
-        console.error('Error getting calendar items: ', err)
+      .catch(error => {
+        console.log(error)
       })
     }
   },
